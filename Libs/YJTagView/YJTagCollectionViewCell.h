7bcf9fb585ba8a7db6368683a175c45cc565278c
刘洋 <18668460187@163.com>
2018-08-06 16:24:57 +0800

    添加列表view

---
+++ Libs/YJTagView/YJTagCollectionViewCell.h
@@ -0,0 +1,19 @@
+//
+//  YJTagCollectionViewCell.h
+//  tagsView
+//
+//  Created by Jake on 2017/6/22.
+//  Copyright © 2017年 Jakey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface YJTagCollectionViewCell : UICollectionViewCell
+
+@property (nonatomic,copy) NSString *content;
+@property (weak, nonatomic) IBOutlet UILabel *tagLabel;
+
+@property (weak, nonatomic) IBOutlet UIImageView *selectedImage;
++ (CGSize) getSizeWithContent:(NSString *) content maxWidth:(CGFloat)maxWidth customHeight:(CGFloat)cellHeight;
+
+@end
